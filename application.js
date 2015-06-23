697d3b3fa967bb047f5717799f5b8664509e9103
Chris Jackson <cjackson290@email.itt-tech.edu>
2015-06-22 22:46:25 -0500

    Working now

---
+++ application.js
@@ -0,0 +1,21 @@
+// alert("Hello world"); 
+function processForm(){
+	var name;
+	var message;
+	var content;
+	name = document.getElementById("name");
+	message = document.getElementById("message");
+	content = document.getElementById("output");
+	
+	//alert("Hello " + name.value + " your message is:" + message.value);
+
+	//content.innerHTML = "Hello " + name.value + " your message is: " + message.value;
+	content.innerHTML = "<div>Hello my name is " + name.value;
+	
+	content.innerHTML += " and you message is" + message.value;
+	content.innerHTML += " and more stuff";
+	content.innerHTML += " yes.. even more</div>";
+	content.innerHTML += "<div>and finally even more stuff!!</div>";
+	
+	
+}	
